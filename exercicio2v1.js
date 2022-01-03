065dac1e866933c7d4fc813d0ea667056d4b3335
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2022-01-03 17:01:53 -0300

    Adicionando exercício 2
    Adicionando 2 versões do exercício 2.

---
+++ exercicio2v1.js
@@ -0,0 +1,13 @@
+function run(person)
+{
+    return person;
+}
+
+let person = 
+{
+    name: 'Batman',
+    power: ''
+}
+
+person.power = "Voar";
+console.log(run(person));
---
+++ exercicio2v2.js
@@ -0,0 +1,13 @@
+let person = 
+{
+    name: 'Batman',
+    power: ''
+}
+person.power = 'Super força';
+
+function run(person)
+{
+    return person;
+}
+
+console.log(run(person));
