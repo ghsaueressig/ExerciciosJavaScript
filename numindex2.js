acf2314e00e30704146622939afec29360a4e8ed
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2022-01-14 16:35:17 -0300

    Adicionando ultimo exercicio do curso de arrays

---
+++ numindex2.js
@@ -0,0 +1,16 @@
+const numeros = [43, 50, 65, 12]
+
+const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+
+//const soma = numeros.reduce(function (acum, atual) {
+// return atual + acum
+//}, 0)
+
+//function operacaoNumerica(acum, atual) 
+//{
+//    return atual + acum
+//}
+   
+//const soma = numeros.reduce(operacaoNumerica, 0)
+
+console.log(soma) //170
